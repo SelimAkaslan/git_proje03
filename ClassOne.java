e55b1af0817066b17c53dbfd7b78fa0ee414e21b
selim akaslan <sakaslan6957@gmail.com>
2020-07-10 18:11:49 +0200

    ikinci commit masterdan

--- ClassOne.java
+++ ClassOne.java
@@ -1,5 +1,9 @@
 package git_proje03;
 
 public class ClassOne {
+	public static void main(String[] args) {
+		System.out.println("Local master branch");
+		System.out.println("Locak master branch ikinci commit");
+	}
 
 }
